60a1391cf033c56b0aee85cea72984a7e9966e0f
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-10-22 11:05:17 +0200

    half done

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,61 @@
-export interface Figure {}
-
-export class Triangle implements Figure {}
-
-export class Circle implements Figure {}
-
-export class Rectangle implements Figure {}
-
-export function getInfo(figure): string {
-  return typeof figure;
+export interface Figure {
+  shape: string;
+  color: string;
 }
+export class Triangle implements Figure {
+  public readonly shape = 'triangle';
+
+  constructor(
+    public color: string,
+    public a: number = 1,
+    public b: number = 1,
+    public c: number = 1,
+  ) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new Error(`sides cannot be equal zero or less`);
+    }
+
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error(`sides 1, 2 and 3 can't form a triangle`);
+    }
+  }
+
+  public getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.round(area * 100) / 100;
+  }
+}
+export class Circle implements Figure {
+  public shape: string = 'circle';
+
+  constructor(
+    public color: string,
+    public radius: number,
+  ) {
+    if (this.radius <= 0) {
+      throw new Error('Radius cannot be equal to zero or less');
+    }
+  }
+
+  public getArea(): number {
+    const area = Math.PI * this.radius ** 2;
+
+    return Math.round(area * 100) / 100;
+  }
+}
+
+// export class Rectangle implements Figure {
+//   public shape: string = 'rectangle';
+
+//   constructor(
+//     public color: string,
+//     public width: number,
+//     public height: number,
+//   ) {}
+// }
+
+// export function getInfo(figure): string {
+//   return typeof figure;
+// }
